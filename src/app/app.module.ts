d9febd3da8e22d81998199bb298b27c211632094
devj <power3xz@gmail.com>
2022-04-10 09:53:23 +0900

    페이지 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DrawableDirective } from './digit/drawable.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DrawableDirective],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
---
+++ src/app/digit/drawable.directive.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appDrawable]',
+})
+export class DrawableDirective {
+  constructor() {}
+}
